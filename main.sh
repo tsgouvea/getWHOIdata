aaa1c282ace8ddece727cde3d8224aa8ec0527a5
Thiago <thiago.gouvea@dfki.de>
2023-11-06 22:26:22 +0100

    Python container instead of Ubuntu

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 srun \
-  --container-image=/netscratch/enroot/ubuntu+20.04.sqsh \
+  --container-image=/netscratch/enroot/python_3.10.sqsh \
   --container-workdir="`pwd`" \
   --container-mounts=/netscratch/$USER:/netscratch/$USER,/ds:/ds:ro,"`pwd`":"`pwd`" \
-  --task-prolog="`pwd`/install.sh" \
   -p A100-IML \
+  --task-prolog="`pwd`/install.sh" \
   python getData.py
